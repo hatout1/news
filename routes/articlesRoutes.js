b7e94a75a4526eccfa37f9285e035e1bae880012
hatout01 <hatout1@gmail.com>
2020-02-10 19:29:43 -0800

    add procfile for heroku

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -75,42 +75,5 @@ router.post("/article", (req, res) => {
 
 })
 
-router.get("/scrape", (req, res) => {
-
-    axios.get("https://www.nytimes.com/section/technology").then(urlencoded => {
-        const $ = cheerio.load(urlencoded.data);
-        $("li.css-ye6x8s").each((i, element) => {
-            let data = {};
-
-            data.link = "https://www.nytimes.com" + $(element)
-                .find('a')
-                .attr("href");
-            data.title = $(element)
-                .find('h2.css-1j9dxys')
-                .text();
-            data.summary = $(element)
-                .find('p.css-1echdzn')
-                .text()
-            data.picture = $(element)
-                .find('img.css-11cwn6f')
-                .attr('src')
-
-            let entry = new Article(data);
-
-            entry.save((err, doc) => {
-                if (err) {
-                    console.log(err);
-                }
-                // Or log the doc
-                else {
-                    console.log(doc);
-                }
-            });
-        });
-        res.send("Scrape Complete");
-    })
-
-
-})
 
 module.exports = router;
